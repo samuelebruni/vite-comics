a5cfa80b59bf8f10c1b5ad1a93f1b6107e7cbb21
samuelebruni <samuele_bruni@hotmail.com>
2023-09-22 16:09:30 +0200

    end components Navbar and add this  at App.vue

---
+++ src/components/componentsNavbar.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+    name: 'componentsNavbar'
+}
+</script>
+
+<template>
+    <navbar id="app_navbar">
+        <div class="container pt-3">
+            <div class="d-flex align-items-center justify-content-between">
+                <div>
+                    <img src="../assets/img/dc-logo.png" alt="">
+                </div>
+                <div>
+                    <ul class="list-unstyled d-flex gap-4 fw-bold">
+                        <li>
+                            CHARACTERS
+                        </li>
+                        <li>
+                            COMICS
+                        </li>
+                        <li>
+                            MOVIES
+                        </li>
+                        <li>
+                            TV
+                        </li>
+                        <li>
+                            GAMES
+                        </li>
+                        <li>
+                            COLLECTIBILES
+                        </li>
+                        <li>
+                            VIDEOS
+                        </li>
+                        <li>
+                            FANS
+                        </li>
+                        <li>
+                            NEWS
+                        </li>
+                        <li>
+                            SHOP
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </navbar>
+</template>
+
+<style lang="scss" scoped></style>
