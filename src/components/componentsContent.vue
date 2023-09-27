b6746b540520d7c03cf0ddd77bccf9631f6f5d58
samuelebruni <samuele_bruni@hotmail.com>
2023-09-27 12:12:06 +0200

    add props in componentsItems and export in componentsContent

--- src/components/componentsContent.vue
+++ src/components/componentsContent.vue
@@ -1,12 +1,14 @@
 <script>
 
 import comics from '../comics'
+import componentsItem from './componentsItem.vue'
 
 export default {
     name: 'componentsContent',
 
     components: {
-        comics
+        comics,
+        componentsItem
     },
 
     data() {
@@ -23,14 +25,7 @@ export default {
         <div class="container">
 
             <div class="row d-flex pb-5">
-                <div class="col-2 mt-5" v-for="item in comics.comics">
-                    <div class="card p-2">
-                        <img :src="item.thumb" class="card-img-top">
-                        <div>
-                            <div class="text-white fw-bold">{{ item.series }}</div>
-                        </div>
-                    </div>
-                </div>
+                <componentsItem :item="item" v-for="item in comics.comics" />
             </div>
             <div class="d-flex align-items-center justify-content-center pb-5">
                 <button class="btn btn-primary rounded-0 fw-bold">LOAD MORE</button>
@@ -42,17 +37,5 @@ export default {
 <style scoped>
 section {
     background-color: black;
-}
-
-img {
-    height: 220px;
-}
-
-.card {
-    height: 250px;
-    background-color: black;
-    border: none;
-    text-transform: uppercase;
-    font-size: 12px;
 }
 </style>
---
+++ src/components/componentsItem.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'componentsItem',
+    props: {
+        item: Object
+    }
+}
+</script>
+
+<template>
+    <div class="col-2 mt-5">
+        <div class="card p-2">
+            <img :src="item.thumb" class="card-img-top">
+            <div>
+                <div class="text-white fw-bold">{{ item.series }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+img {
+    height: 220px;
+}
+
+.card {
+    height: 250px;
+    background-color: black;
+    border: none;
+    text-transform: uppercase;
+    font-size: 12px;
+}
+</style>
